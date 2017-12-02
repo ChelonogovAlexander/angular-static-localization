be4ecfe884a8cd969c029f0b7d1976930364abfa
ChelonogovAlexander <ChelonogovAlexander@gmail.com>
2017-12-02 19:47:23 +0300

    Select Language start...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StaticLocalizationService } from './shared/static-localization.service';
 import { Subject } from 'rxjs/Subject';
 import { Subscription } from 'rxjs/Subscription';
+import { Culture } from './shared/culture';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,20 @@ import { Subscription } from 'rxjs/Subscription';
 export class AppComponent implements OnInit, OnDestroy {
   private translateSubscription: Subscription;
   public resources: any;
+  public languages = [{
+    value: Culture.Russian,
+    name: 'Русский'
+}, {
+  value: Culture.English,
+  name: 'English'
+}, {
+  value: Culture.German,
+  name: 'German'
+}, {
+  value: Culture.French,
+  name: 'French'
+}];
+
   title = 'app';
 
   constructor(private staticLocalizationService: StaticLocalizationService) {
@@ -24,5 +39,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.translateSubscription.unsubscribe();
+  }
+
+  onChangeLanguage(newValue: number) {
+    console.log(newValue);
+    // let culture = Culture[newValue];
   }
 }
